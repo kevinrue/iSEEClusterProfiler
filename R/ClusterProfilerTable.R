45057d357a518f1f91efb697016d58e58587bd71
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-06-03 18:12:40 +0100

    add documentation

--- R/ClusterProfilerTable.R
+++ R/ClusterProfilerTable.R
@@ -1,3 +1,40 @@
+#' clusterProfiler Result Table
+#'
+#' A table that dynamically computes pathway analysis results for a given gene set.
+#'
+#' @section Slot overview:
+#' This class does not define any additional slots yet.
+#'
+#' This class inherits all slots from its parent
+#' `Table-class` and `Panel-class` classes.
+#'
+#' @section Constructor:
+#' `ClusterProfilerTable(...)` creates an instance of a ClusterProfilerTable class,
+#' where any slot and its value can be passed to `...` as a named argument.
+#'
+#' @section Supported methods:
+#' In the following code snippets, `x` is an instance of a [ClusterProfilerTable-class] class.
+#' Refer to the documentation for each method for more details on the remaining arguments.
+#'
+#' For defining the interface:
+#'
+#' - `.fullName(x)` will return `"clusterProfiler Table"`.
+#' - `.hideInterface(x)` will return \code{TRUE} for UI elements related to multiple column selections,
+#' otherwise calling the method for \linkS4class{RowTable}.
+#' - `.panelColor(x)` will return the specified default color for this panel class.
+#'
+#' For creating the table:
+#'
+#' - `.generateTable(x, envir)` will create a data.frame of newly computed statistics in \code{envir}.
+#'   The method will return the commands required to do so.
+#'
+#' @name ClusterProfilerTable-class
+#' @aliases ClusterProfilerTable ClusterProfilerTable-class
+#' .fullName,ClusterProfilerTable-method
+#' .panelColor,ClusterProfilerTable-method
+#' .generateTable,ClusterProfilerTable-method
+#' .hideInterface,ClusterProfilerTable-method
+NULL
 
 #' @export
 setClass("ClusterProfilerTable", contains="Table")
@@ -19,12 +56,15 @@ ClusterProfilerTable <- function(...) {
 }
 
 #' @export
+#' @importMethodsFrom iSEE .fullName
 setMethod(".fullName", "ClusterProfilerTable", function(x) "clusterProfiler Table")
 
 #' @export
+#' @importMethodsFrom iSEE .panelColor
 setMethod(".panelColor", "ClusterProfilerTable", function(x) "#111111")
 
 #' @export
+#' @importMethodsFrom iSEE .generateTable
 #' @importFrom iSEE .textEval
 setMethod(".generateTable", "ClusterProfilerTable", function (x, envir) {
   cmds <- .create_enrichgo_table(envir)
@@ -35,6 +75,7 @@ setMethod(".generateTable", "ClusterProfilerTable", function (x, envir) {
 )
 
 #' @export
+#' @importMethodsFrom iSEE .hideInterface
 setMethod(".hideInterface", "ClusterProfilerTable", function(x, field) {
   if (field %in% c(
     "ColumnSelectionSource",
